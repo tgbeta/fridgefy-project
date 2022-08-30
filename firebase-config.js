9b9fce83ff9d2efa23e6ca16fc9761f0c9f1ac5f
Megumi Akama <kamayatu91@gmail.com>
2022-08-30 13:54:38 -0700

    Add firebase

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHgmt8x82k1FwswIztOmCqkjpCc3l66OI",
+  authDomain: "fridgefy-e1ca1.firebaseapp.com",
+  projectId: "fridgefy-e1ca1",
+  storageBucket: "fridgefy-e1ca1.appspot.com",
+  messagingSenderId: "651465515458",
+  appId: "1:651465515458:web:44cff17e29651743a2b65b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
