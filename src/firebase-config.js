9b5745a5fcdb227308eec582ad5de31a5f43833c
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-09-02 16:13:49 -0700

    json update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,8 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-// export const db = getFirestore();
-
-
 export const db = getFirestore();
 
