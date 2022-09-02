eb8d422dfeea531d05b100811534c1a58dd2f2dc
Tatiana B <59037000+tgbeta@users.noreply.github.com>
2022-09-02 16:25:17 -0400

    Add files via upload

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FB_APPID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENTID
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
