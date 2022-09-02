2938c20153b05962c73e4cf6f2ae0e0527db8d76
Tatiana <tatianabohrer@gmail.com>
2022-09-02 16:20:04 -0400

    add firebase file

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAdhW8WR3Cfo_ZOsUGUQGMDT0731GXF6E8",
-  authDomain: "fridgefy-project.firebaseapp.com",
-  projectId: "fridgefy-project",
-  storageBucket: "fridgefy-project.appspot.com",
-  messagingSenderId: "833037880722",
-  appId: "1:833037880722:web:110db550e61d462c3cc54b",
-  measurementId: "G-TY3DWW4KNL"
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FB_APPID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENTID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
